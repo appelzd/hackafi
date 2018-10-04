1c2b2008cf17a7c644183537c20ff2ab73a81aa8
David Appel <appel.david@outlook.com>
2018-10-04 09:09:16 -0600

    added comments

--- Orthofi.OCR.Repository/InsuranceRepository.cs
+++ Orthofi.OCR.Repository/InsuranceRepository.cs
@@ -6,6 +6,16 @@ namespace Orthofi.OCR.Repository
 {
     public class InsuranceRepository
     {
+
+        //need a list of insurance carriers (id and name only)
+
+
+        //need a list of InsuranceGroupid, Planname, GroupName from InsuranceGroup 
+        
+        //need a list of policyid, first name, last name from InsurancePolicy
+
+
+
         //public IList<DtoInsuranceCarrier> GetInsurances()
         //{
         //    string query = "select InsuranceCarrierId as ID, name from InsuranceCarrier as Name";
